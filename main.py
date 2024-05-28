d971b0e9f1498fd8efbaf37cffde6419a9b31e0d
spnc1 <cooley.exe@gmail.com>
2024-05-28 21:31:28 +1000

    doc strings and readability changes

--- main.py
+++ main.py
@@ -6,27 +6,53 @@ elif os.name == 'posix': os.system('clear')
 data = []
 answers = []
 
-def readCsv(filename, targetArray, answersTargetArray):
-    with open(filename, mode="r") as csv_file: #"r" represents the read mode
+def readCsv(filepath: str, targetArray: list, answersTargetArray: list):
+    """
+    Takes a file path of training data and saves it to 2 arrays, a data and an answers array.
+    IGNORES HEADERS AND MUST BE FLOATS
+
+    Paramaters
+    ----------
+    filepath            : File path of training/testing data to be read
+    targetArray         : Name of arrray for training/testing data
+    answersTargetArray  : Name of array for training/testing data answers
+    """
+
+    with open(filepath, mode="r") as csv_file:
         reader = csv.reader(csv_file)
+
+        # skips header
         next(reader)
+
+        # QUOTE_NONNUMERIC converts all non-quoted elements to floats
         reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
+
         for item in reader:
             targetArray.append(item[1:])
             answersTargetArray.append(item[0])
 
-def forwardPropagation(input, activationFunction):
+def activationLayer(input: list[int], activationFunction: str) -> list:
+    """
+    Takes a layer and applies a specified activation function to the entire layer, outputting the activated layer
+
+    Parameters
+    ----------
+    input               : Unactivated layer needing to be passed through an activation layer
+    activationFunction  : Choice of activation function (ReLU, Leaky ReLU, Logistic Sigmoid, Softmax)
+
+    Returns
+    -------
+    Layer activated with specified function
+    """
+
     if activationFunction == 'ReLU':
-        for i in range(len(input)):
-            input[i] = max(0, input[i])
+        for i in range(len(input)): input[i] = max(0, input[i])
     
     elif activationFunction == 'Leaky ReLU':
-        for i in range(len(input)):
-            input[i] = max(0.01*input[i], input[i])
+        for i in range(len(input)): input[i] = max(0.01*input[i], input[i])
     
     elif activationFunction == 'Logistic Sigmoid':
-        for i in range(len(input)):
-            input[i] = 1/(1+math.exp(-input[i]))
+        for i in range(len(input)): input[i] = 1/(1+math.exp(-input[i]))
     
     elif activationFunction == 'Softmax':
         allExponentials = []
@@ -35,53 +61,77 @@ def forwardPropagation(input, activationFunction):
             ea = math.exp(input[i])
             allExponentials.append(ea)
             allExponentialsSum += ea
-        for i in range(len(input)):
-            input[i] = allExponentials[i]/allExponentialsSum
+        for i in range(len(input)): input[i] = allExponentials[i]/allExponentialsSum
 
     return input
 
 class Layer():
-    def __init__(self, nInputs, nNeurons):
+    """
+    A class to represent a hidden or output layer of a neural network
 
-        # generate matrix of weights
+    Attributes
+    ----------
+    nInputs             : The amount of neurons in the previous layer (Size of input layer)
+    nNeurons            : The amount of Neurons in the current layer (Size of this layer)
+    weights             : The weight matrix
+    biases              : The bias vector
+    output              : The layer output
+
+    Methods
+    -------
+    forwardPropagation  : Takes an input layer and an activation function choice and saves the output to self.output
+    """
+
+    def __init__(self, nInputs: int, nNeurons: int):
+
+        # Generate a matrix of weights with nInputs width and nNeurons height
         weights = []
         for i in range(nNeurons):
             weightsBatch = []
-            for x in range(nInputs):
-                weightsBatch.append(0.025*random.uniform(-0.5,0.5))
+            for x in range(nInputs): weightsBatch.append(0.025*random.uniform(-0.5,0.5))
             weights.append(weightsBatch)
         
-        # generate vector of biases
+        # Generate a vector of biases with length nNeurons
         biases = []
-        for i in range(nNeurons):
-            biases.append(random.uniform(-0.5,0.5))
+        for i in range(nNeurons): biases.append(random.uniform(-0.5,0.5))
 
-        # store values
+        # Make them accessible by the class object
         self.nInputs = nInputs
         self.nNeurons = nNeurons
         self.weights = weights
         self.biases = biases
     
-    def forwardPropagation(self, inputs, activationFunction):
+    def forwardPropagation(self, inputs: list[int], activationFunction: str):
+        """
+        Method for forward propagation of the neural network at the current layer
+
+        Parameters
+        ----------
+        inputs              : The input layer
+        activationFunction  : Choice of activation function (ReLU, Leaky ReLU, Logistic Sigmoid, Softmax)
+
+        Outputs
+        --------
+        self.output         : The vector output of the layer, accessible with self.output
+        """
         output = []
         
         for i in range(self.nNeurons):
             outputBatch = 0
-            for x in range(len(inputs)):
-                outputBatch += inputs[x] * self.weights[i][x]
+            for x in range(len(inputs)): outputBatch += inputs[x] * self.weights[i][x]
             output.append(outputBatch + self.biases[i])
-
-        self.output = forwardPropagation(output, activationFunction)
+        
+        self.output = activationLayer(output, activationFunction)
 
 inputLayer = [255,84,0,0,15]
 
-h1 = Layer(len(inputLayer),10)
+h1 = Layer(len(inputLayer),5)
 h1.forwardPropagation(inputLayer, 'Logistic Sigmoid')
 
-h2 = Layer(len(h1.output),10)
+h2 = Layer(len(h1.output),5)
 h2.forwardPropagation(h1.output, 'ReLU')
 
-outputLayer = Layer(len(h2.output), 10)
+outputLayer = Layer(len(h2.output), 2)
 outputLayer.forwardPropagation(h2.output, 'Softmax')
 
 print('Input Layer')
