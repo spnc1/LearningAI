6f89eae692f23478a3c6e39afd71769cb17b8a22
spnc1 <cooley.exe@gmail.com>
2024-07-18 22:30:34 +1000

    Finished the project now experimenting with learning rates

--- MNIST.py
+++ MNIST.py
@@ -53,21 +53,23 @@ def stableSoftMax(DataFrame):
 def dSoftMax(DataFrame):
     s = SoftMax(DataFrame)
     return s * (1 - s)
+def dStableSoftMax(DataFrame):
+    s = stableSoftMax(DataFrame)
+    return s * (1 - s)
 
 def DataFrameOneHot(labels): return pd.DataFrame([oneHot(label, 10) for label in labels.values[0]]).T
 
-def forwardPropagation(Z0, W1, W2, b1, b2):
+def forwardPropagation(Z0, W1, b1, W2, b2):
     # Apply weights and biases with ReLU Activation
     Z1 = W1 @ Z0 + b1
     A1 = LeakyReLU(Z1)
     Z2 = W2 @ A1 + b2
-    A2 = SoftMax(Z2)
-
+    A2 = stableSoftMax(Z2)
     return Z1, A1, Z2, A2
 
 def backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y):
     # dZ2 10x1 | dW2 10x16
-    dZ2 = (2 * (A2 - Y) / len(A2)) * dSoftMax(Z2)
+    dZ2 = (2 * (A2 - Y) / len(A2)) * dStableSoftMax(Z2)
     dW2 = dZ2 @ A1.T
     
     # dZ1 16x1 | dW1 16x784
@@ -76,19 +78,50 @@ def backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y):
 
     return dW1, dZ1, dW2, dZ2
 
-def updateParameters(W1, b1, W2, b2, alpha, dW1, db1, dW2, db2):
+def updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
     b1 = b1 - alpha * db1
     W2 = W2 - alpha * dW2
     b2 = b2 - alpha * db2
+
     return W1, b1, W2, b2
 
-labels, data = getData(MNISTdata.testingData)
+labels, data = getData(MNISTdata.trainingData)
+testLabels, testData = getData(MNISTdata.testingData)
 labels = DataFrameOneHot(labels)
-Y = pd.DataFrame(labels.iloc[:,0]).reset_index(drop=True)
+testLabels = DataFrameOneHot(testLabels)
 
-W1, b1, W2, b2 = initialiseParameters()
-Z0 = pd.DataFrame(data.iloc[:, 0]).reset_index(drop=True)
-Z1, A1, Z2, A2 = forwardPropagation(Z0, W1, W2, b1, b2)
+def gradientDescent(data, labels, alpha, epochs):
+    W1, b1, W2, b2 = initialiseParameters()
+    for epoch in range(epochs):
+        for i in range(len(labels.columns)):
+            Y = pd.DataFrame(labels.iloc[:,i]).reset_index(drop=True)
+            Y.columns = [0]
+            Z0 = pd.DataFrame(data.iloc[:,i]).reset_index(drop=True)
+            Z0.columns = [0]
 
-dW1, db1, dW2, db2 = backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y)
+            Z1, A1, Z2, A2 = forwardPropagation(Z0, W1, b1, W2, b2)
+            dW1, db1, dW2, db2 = backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y)
+            W1, b1, W2, b2 = updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+            
+            if i % 1000 == 0:
+                print(f'Epoch: {epoch + 1} | Image: {i}\nPredicted {A2.idxmax().iloc[0]} for {Y.idxmax().iloc[0]}\n')
+    
+    print('Training Complete')
+    return W1, b1, W2, b2
+
+def testModel(testLabels, testData, W1, b1, W2, b2):
+    correct = 0
+    for i in range(len(testLabels.columns)):
+        Y = pd.DataFrame(testLabels.iloc[:,i]).reset_index(drop=True)
+        Y.columns = [0]
+        Z0 = pd.DataFrame(testData.iloc[:,i]).reset_index(drop=True)
+        Z0.columns = [0]
+        
+        Z1, A1, Z2, A2 = forwardPropagation(Z0, W1, b1, W2, b2)
+        correct += 1 if A2.idxmax().iloc[0] == Y.idxmax().iloc[0] else 0
+    
+    return correct/len(testLabels.columns)
+
+W1, b1, W2, b2 = gradientDescent(data, labels, 0.075, epochs=1)
+print(f'\nAccuracy on testing data: {testModel(testLabels, testData, W1, b1, W2, b2) * 100}%')
