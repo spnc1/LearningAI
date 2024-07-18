4a64780a830a354cdbf4903861ac3ba1df44b074
spnc1 <cooley.exe@gmail.com>
2024-07-18 21:12:24 +1000

    Implemented backpropagation and parameter updating functions

--- MNIST.py
+++ MNIST.py
@@ -22,18 +22,17 @@ Get Column ↓
 def initialiseParameters():
     # Generate 16x784 Weight Matrix & 16x1 Bias Matrix
     W1 = pd.DataFrame([[random.uniform(-0.5, 0.5) for column in range(784)] for row in range(16)])
-    B1 = pd.DataFrame([random.uniform(-0.5, 0.5) for row in range(16)], columns=[0])
+    b1 = pd.DataFrame([random.uniform(-0.5, 0.5) for row in range(16)], columns=[0])
 
     # Generate 10x16 Weight Matrix & 10x1 Bias Matrix
     W2 = pd.DataFrame([[random.uniform(-0.5, 0.5) for column in range(16)] for row in range(10)])
-    B2 = pd.DataFrame([random.uniform(-0.5, 0.5) for row in range(10)], columns=[0])
+    b2 = pd.DataFrame([random.uniform(-0.5, 0.5) for row in range(10)], columns=[0])
 
-    return W1, B1, W2, B2
+    return W1, b1, W2, b2
 
 def ReLU(DataFrame): return DataFrame * (DataFrame > 0)
-def dRelu(DataFrame): return 1 * (DataFrame > 0)
-
 def LeakyReLU(DataFrame): return DataFrame * (DataFrame > 0) + (0.1 * DataFrame * (DataFrame < 0))
+def dRelu(DataFrame): return 1 * (DataFrame > 0)
 def dLeakyReLU(DataFrame): return 1 * (DataFrame > 0) + (0.1 * (DataFrame < 0))
 
 def SoftMax(DataFrame):
@@ -51,36 +50,45 @@ def stableSoftMax(DataFrame):
     for col in DataFrame.columns: dataFrameExp[col] = DataFrame[col].apply(lambda x: math.exp(x - dataFrameMax[col]))
     sumExp = dataFrameExp.sum(axis=0)
     return dataFrameExp.div(sumExp, axis=1)
-def dSoftMax(DataFrame): return DataFrame * (1 - DataFrame)
+def dSoftMax(DataFrame):
+    s = SoftMax(DataFrame)
+    return s * (1 - s)
 
 def DataFrameOneHot(labels): return pd.DataFrame([oneHot(label, 10) for label in labels.values[0]]).T
 
-def forwardPropagation(Z0, W1, W2, B1, B2):
+def forwardPropagation(Z0, W1, W2, b1, b2):
     # Apply weights and biases with ReLU Activation
-    Z1 = W1 @ Z0 + B1
+    Z1 = W1 @ Z0 + b1
     A1 = LeakyReLU(Z1)
-    Z2 = W2 @ A1 + B2
+    Z2 = W2 @ A1 + b2
     A2 = SoftMax(Z2)
 
     return Z1, A1, Z2, A2
 
-def backwardPropagation(Z1, W1, B1, A1, Z2, W2, B2, A2, Y):
-    y = pd.DataFrame(Y.iloc[:,0])
-    dZ2 = 2 * (A2 - y) * dSoftMax(A2)
+def backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y):
+    # dZ2 10x1 | dW2 10x16
+    dZ2 = (2 * (A2 - Y) / len(A2)) * dSoftMax(Z2)
+    dW2 = dZ2 @ A1.T
+    
+    # dZ1 16x1 | dW1 16x784
+    dZ1 = W2.T @ dZ2 * dLeakyReLU(Z1)
+    dW1 = dZ1 @ Z0.T
 
-    dW2 = dZ2 * A1
-    dB2 = dZ2
+    return dW1, dZ1, dW2, dZ2
 
-    print(dZ2)
+def updateParameters(W1, b1, W2, b2, alpha, dW1, db1, dW2, db2):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
 
 labels, data = getData(MNISTdata.testingData)
+labels = DataFrameOneHot(labels)
+Y = pd.DataFrame(labels.iloc[:,0]).reset_index(drop=True)
 
-W1, B1, W2, B2 = initialiseParameters()
+W1, b1, W2, b2 = initialiseParameters()
 Z0 = pd.DataFrame(data.iloc[:, 0]).reset_index(drop=True)
-Z1, A1, Z2, A2 = forwardPropagation(Z0, W1, W2, B1, B2)
+Z1, A1, Z2, A2 = forwardPropagation(Z0, W1, W2, b1, b2)
 
-Y = DataFrameOneHot(labels)
-# print(Y)
-# Y.columns = ['', '0']
-
-backwardPropagation(Z1, W1, B1, A1, Z2, W2, B2, A2, Y)
+dW1, db1, dW2, db2 = backwardPropagation(Z0, Z1, A1, Z2, W2, A2, Y)
