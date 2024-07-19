72b4a781601f77cf20ad55d189acd09cba466fbf
spnc1 <cooley.exe@gmail.com>
2024-07-19 21:59:23 +1000

    Complete now conducting learning rate tests

--- MNIST.py
+++ MNIST.py
@@ -15,8 +15,8 @@ def oneHot(array, oneHotLength):
 
 # TrainingY (60000x1) | TrainingX (784x60000)
 # TestingY (10000x1) | TestingX (784x10000)
-# trainingY, trainingX = getData(MNISTdata.trainingData)
-# trainingY = oneHot(trainingY, 10)
+trainingY, trainingX = getData(MNISTdata.trainingData)
+trainingY = oneHot(trainingY, 10)
 testingY, testingX = getData(MNISTdata.testingData)
 testingY = oneHot(testingY, 10)
 
@@ -35,35 +35,62 @@ def dStableSoftMax(array): return StableSoftMax(array) * (1 - StableSoftMax(arra
 
 def initialiseParameters():
     # Generate 16x784 Weight Matrix & 16x1 Bias Matrix
-    W1 = np.random.rand(16, 784) - 0.5
-    b1 = np.random.rand(16, 1) - 0.5
+    W1 = np.random.uniform(-0.5, 0.5, (16, 784))
+    b1 = np.random.uniform(-0.5, 0.5, (16, 1))
 
     # Generate 10x16 Weight Matrix & 10x1 Bias Matrix
-    W2 = np.random.rand(10, 16) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.uniform(-0.5, 0.5, (10, 16))
+    b2 = np.random.uniform(-0.5, 0.5, (10, 1))
 
     return W1, b1, W2, b2
 
 def forwardPropagation(W1, b1, W2, b2, Z0):
-    # Z0 currently a row vector not a column vector
-    Z0 = Z0.reshape(784,1)
-
     Z1 = W1 @ Z0 + b1
     A1 = LeakyReLU(Z1, 0.1)
     Z2 = W2 @ Z1 + b2
     A2 = StableSoftMax(Z2)
     return Z0, Z1, A1, Z2, A2
 
-def backPropagation(Z0, Z1, A1, A2, W1, W2, Y):
-    # Y currently a row vector not a column vector
-    Y = Y.reshape(10,1)
-
+def backPropagation(Z0, Z1, A1, A2, W2, Y):
     dZ2 = A2 - Y
     dW2 = dZ2 @ A1.T
-    dZ1 = W2.T @ dZ2 * dLeakyReLU(Z1, 0.1)
+    dZ1 = (W2.T @ dZ2) * dLeakyReLU(Z1, 0.1)
     dW1 = dZ1 @ Z0.T
     return dW1, dZ1, dW2, dZ2
 
-W1, b1, W2, b2 = initialiseParameters()
-Z0, Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, testingX[:, 0])
-dW1, db1, dW2, db2 = backPropagation(Z0, Z1, A1, A2, W1, W2, testingY[:,0])
+def updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, learningRate):
+    W1 = W1 - learningRate * dW1
+    b1 = b1 - learningRate * db1
+    W2 = W2 - learningRate * dW2
+    b2 = b2 - learningRate * db2
+
+    return W1, b1, W2, b2
+
+def testModel(data, labels, W1, b1, W2, b2):
+    count = 0
+    for i in range(data.shape[1]):
+        testX = data[:, i].reshape(784, 1)
+        testY = labels[:, i].reshape(10, 1)
+        Z0, Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, testX)
+
+        if np.argmax(testY, 0)[0] == np.argmax(A2, 0)[0]: count += 1
+    
+    return count / data.shape[1]
+
+baseW1, baseb1, baseW2, baseb2 = initialiseParameters()
+
+def gradientDescent(W1, b1, W2, b2, data, labels, learningRate, epochs):
+    for epoch in range(epochs):
+        for i in range(data.shape[1]):
+            X = data[:, i].reshape(784, 1)
+            Y = labels[:, i].reshape(10, 1)
+            Z0, Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, X)
+            dW1, db1, dW2, db2 = backPropagation(Z0, Z1, A1, A2, W2, Y)
+            W1, b1, W2, b2 = updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, learningRate)
+
+            if i % 10000 == 0: print(f'Epoch: {epoch + 1} | Iteration : {i}\n{np.argmax(Y, 0)[0]} -> Predicted {np.argmax(A2, 0)[0]}\n')
+    
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradientDescent(baseW1, baseb1, baseW2, baseb2, trainingX, trainingY, 0.04, 1)
+print(f'Accuracy on testing data: {testModel(testingX, testingY, W1, b1, W2, b2) * 100}%')
